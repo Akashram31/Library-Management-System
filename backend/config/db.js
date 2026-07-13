a8bbb889e3937ebbf42b06e66ce1dc153233241b
Akashram31 <ramakash922@gmail.com>
2026-07-13 23:20:05 +0530

    Configure MongoDB Atlas connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,23 +1,17 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+    try {
+        console.log("MONGO_URI:", process.env.MONGO_URI);
 
-    try{
+        await mongoose.connect(process.env.MONGO_URI);
 
-        await mongoose.connect(
-            'mongodb://127.0.0.1:27017/librarysystem'
-        );
-
-        console.log('MongoDB Connected Successfully');
-
-    }catch(err){
-
-        console.log('Database Connection Failed : ' + err.message);
-
+        console.log("MongoDB Connected Successfully");
+    } catch (err) {
+        console.log("Database Connection Failed:");
+        console.log(err);
         process.exit(1);
-
     }
-
 };
 
 module.exports = connectDB;
