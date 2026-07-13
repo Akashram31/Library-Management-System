09e0d6b52c14cf7cdab61a3fff858c223ea70a91
Akashram31 <ramakash922@gmail.com>
2026-07-14 00:06:29 +0530

    Increase MongoDB timeout

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,18 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 30000,
-    });
+    console.log("Connecting to MongoDB...");
+    console.log(process.env.MONGO_URI);
+
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected Successfully");
   } catch (err) {
+    console.error("========== FULL ERROR ==========");
     console.error(err);
+    console.error("Reason:", err.reason);
+    console.error("Cause:", err.cause);
+    console.error("================================");
     process.exit(1);
   }
 };
